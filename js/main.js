9b0b6e7be4e1ecaea7ba2dc7f4e8eff20f3afedf
Tomasz Wiklo <wikweb@gmail.com>
2015-09-26 19:08:07 +0200

    Version with one page scroll

---
+++ js/main.js
@@ -0,0 +1,47 @@
+  // Page scroller
+(function() {
+  var delay = false;
+
+  $(document).on('mousewheel DOMMouseScroll', function(event) {
+    event.preventDefault();
+    if(delay) return;
+
+    delay = true;
+    setTimeout(function(){delay = false},200)
+
+    var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+
+    var a= document.getElementsByTagName('section');
+    if(wd < 0) {
+      for(var i = 0 ; i < a.length ; i++) {
+        var t = a[i].getClientRects()[0].top;
+        if(t >= 40) break;
+      }
+    }
+    else {
+      for(var i = a.length-1 ; i >= 0 ; i--) {
+        var t = a[i].getClientRects()[0].top;
+        if(t < -20) break;
+      }
+    }
+    $('html,body').animate({
+      scrollTop: a[i].offsetTop
+    });
+  });
+})();
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
